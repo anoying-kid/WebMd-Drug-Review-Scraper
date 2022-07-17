b6d422c5b4492e7192e6ee33546aadf85f6b641a
anoying-kid <yugrana853@gmail.com>
2022-07-17 10:25:18 +0530

    Solved old issues

--- extractor.py
+++ extractor.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
-from csv import DictWriter, DictReader, writer
+from csv import writer
 import requests
 import re
+import concurrent.futures as cf
 # from webscrapper import WebmdScraper
 
 def age_sex(soup):
@@ -40,11 +41,10 @@ def description(soup):
     return soup
 
 def drug_drugid(url):
-    r = re.search(r'https://www.webmd.com/drugs/drugreview-(.*?)-(.*?)', url)
-    drug_id = r.group(1)
-    n = url.find(drug_id)
-    drug = url[len(drug_id)+n+1:]
-    return (drug, drug_id)
+    r = re.findall(r'https://www.webmd.com/drugs/drugreview-(.*?)-(.*?).aspx\?drugid=', url)
+    if r:
+        return r[0]
+    return (None, None)
 
 def extracting_data_from_review(soup):
     age,sex = age_sex(soup)
@@ -55,35 +55,41 @@ def extracting_data_from_review(soup):
         reviews = description(soup)
         return [age, sex, cond, rate, eff, ease, satis, reviews]
 
-def get_right_url(url, headers):
-    r = requests.get(url, headers=headers)
-    url = r.url
-    r.close()
-    return url
+def write_csv(s, page, till_page, url, headers, drug, drug_id):
+    query = f"?conditionid=&sortval=1&pagenumber={page}"
+    try:
+        r = s.get(url+query, headers=headers)
+    except:
+        print('\n')
+        print(query)
+        print('\n')
+        # return 0
+    else:
+        soup = BeautifulSoup(r.text, 'lxml')
+        r = soup.find_all('div', {'class':'review-details'})
+        for num in r:
+            ls = extracting_data_from_review(num)
+            if ls:
+                ls.extend([drug, drug_id])
+                with open('webmd.csv', 'a', newline='') as f:
+                    csv_writer = writer(f)
+                    csv_writer.writerow(ls)
 
 def getting_all_reviews(url, reviews, headers):
+    url = "https://www.webmd.com"+url
+    print(f"fetching url {url}")
     till_page = reviews/20
     till_page = (till_page//1)+1 if (till_page%1>0) else till_page//1
     till_page = int(till_page)
-    drug, drug_id = drug_drugid(url)
-    for page in range(0,till_page):
-        print(page, till_page, url)
-        query = f"?conditionid=&sortval=1&pagenumber={page}"
-        with requests.session() as s:
-            r = s.get(url+query, headers=headers)
-            soup = BeautifulSoup(r.text, 'lxml')
-            r = soup.find_all('div', {'class':'review-details'})
-            # print(len(r))
-            for num in r:
-                ls = extracting_data_from_review(num)
-                if ls:
-                    ls.extend([drug, drug_id])
-                    with open('webmd.csv', 'a', newline='') as f:
-                        csv_writer = writer(f)
-                        csv_writer.writerow(ls)
+    drug_id, drug = drug_drugid(url)
+    with cf.ThreadPoolExecutor() as executor:
+        with requests.Session() as s:
+            results = [executor.submit(write_csv, s, page, till_page, url, headers, drug, drug_id) for page in range(0,till_page)]
+            for res in cf.as_completed(results):
+                res.result()
 
 if __name__ == '__main__':
     headers= {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"}
-    url = "https://www.webmd.com/drugs/drugreview-64439-abilify"
-    reviews = 1837
+    url = "/drugs/drugreview-20512-abreva-topical?drugid=20512&drugname=abreva-cream"
+    reviews = 144
     getting_all_reviews(url, reviews, headers)
--- webscrapper.py
+++ webscrapper.py
@@ -1,67 +1,63 @@
 from bs4 import BeautifulSoup
 from extractor import getting_all_reviews
-from csv import DictWriter
+from csv import writer
 import requests
 import string
 import re
+import concurrent.futures as cf
+import asyncio
 
-class WebmdScraper():
+class Webmd():
     url = "https://www.webmd.com/drugs/2/alpha/"
     loc = [f'{s}/{s}{t}' for s in string.ascii_lowercase for t in string.ascii_lowercase ]
     # loc = ['a/aa','a/ab','b/ba','b/bb']
     headers= {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"}
 
-    def request_data(self):
-        for var in self.loc:
+    def getting_drug_links(self, var):
+        with requests.session() as s:
             new_link = self.url+var
-            with requests.session() as s:
-                r = s.get(new_link, headers=self.headers)
-                yield r.text
-    
-    def get_links(self):
-        for r in self.request_data():
-            soup = BeautifulSoup(r, 'lxml')
+            r = s.get(new_link, headers=self.headers)
+            soup = BeautifulSoup(r.text, 'lxml')
             r = soup.find_all('a', {'class': 'alpha-drug-name'})
-            for i in r:
-                yield i['href']
-    
-    def entering_links(self):
-        url = "https://www.webmd.com"
-        for link_direct in self.get_links():
-            new_link = url + link_direct
-            with requests.session() as s:
-                r = s.get(new_link, headers=self.headers)
-                yield (r.text,new_link)
+            return [i['href'] for i in r]
 
-    def number_of_review(self,soup):
-        r = re.findall(r'\(.*?\)', soup.text)
+    def number_of_review(self,r):
+        r = re.findall(r'\(.*?\)', r.text)
         r = r[0]
         r = r[1:-1]
         return int(r)
 
-    def getting_review_links(self):
-        for r,new_link in self.entering_links():
-            soup = BeautifulSoup(r, 'lxml')
-            r = soup.find('ul', {'class':'auto-tabs'})
-            if r:
-                r = r('li')[-1]
-                reviews = self.number_of_review(r)
-                if (reviews!=0):
-                    r = r.a['href']
-                    yield (r,reviews)
-            else:
-                print(new_link)
+    def review_links(self, s, site):
+        r = s.get(site, headers=self.headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        r = soup.find('ul', {'class':'auto-tabs'})
+        if r:
+            r = r('li')[-1]
+            reviews = self.number_of_review(r)
+            if (reviews!=0):
+                r = r.a['href']
+                getting_all_reviews(r, reviews, self.headers)
 
-    def extracting_review_from_page(self):
-        for link,reviews in self.getting_review_links():
-            if link:
-                with open('webmd.csv', 'a') as f:
-                    head = ['Age','Sex','Condition','Rating','Effectiveness','Ease of use', 'Satisfaction', 'Reviews', 'Drug', 'DrugId']
-                    csv_writer = DictWriter(f, fieldnames=head)
-                    if f.tell() == 0:
-                        csv_writer.writeheader()
-                getting_all_reviews(link[:-5], int(reviews), self.headers)
+    async def getting_review_links(self, var):
+        links = self.getting_drug_links(var)
+        url = "https://www.webmd.com"
+        with requests.session() as s:
+            with cf.ThreadPoolExecutor() as executor:
+                results = [executor.submit(self.review_links, s, url+link) for link in links]
+            for res in cf.as_completed(results):
+                res.result()
+
+    async def for_all_alphabets(self):
+        for var in self.loc:
+            await self.getting_review_links(var)
+
+    def main(self):
+        with open('webmd.csv', 'a', newline='') as f:
+            head = ['Age','Sex','Condition','Rating','Effectiveness','Ease of use', 'Satisfaction', 'Reviews', 'Drug', 'DrugId']
+            csv_writer = writer(f)
+            csv_writer.writerow(head)
+        asyncio.run(self.for_all_alphabets())
 
 if __name__ == '__main__':
-    webmd = WebmdScraper()
-    webmd.extracting_review_from_page()
+    web = Webmd()
+    web.main()
